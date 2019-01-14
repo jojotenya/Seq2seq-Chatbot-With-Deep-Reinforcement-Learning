7dba97c8532f14efa9d92581c0e30cae77c1238c
jojotenya <jojotenya@gmail.com>
2019-01-14 19:54:34 +0800

    dump MLE performance

---
+++ dump_ave_vals.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+import pandas as pd
+import matplotlib as mpl
+mpl.use('Agg')
+
+model_dir = os.environ["model_dir"]
+loss_val_file = os.path.join(model_dir,"loss_val")
+loss_train_file = os.path.join(model_dir,"loss_train")
+limit_to = 200
+
+with open(loss_val_file, "r") as f:
+    ave = 0
+    vals = []
+    for i,d in enumerate(f.readlines()):
+        if i !=0 and (i+1)%5==0:
+            vals.append(ave/4.0)
+            ave=0
+        else:
+            ave+=float(d)
+
+with open(loss_train_file, "r") as f:
+    trains = [float(row.strip()) for row in f.readlines()] 
+
+print('model_dir: ',model_dir)
+df = pd.DataFrame({"train":trains[:limit_to],"val":vals[:limit_to]})
+p = df.plot()
+fig = p.get_figure()
+fig.savefig('outputs/figures/%s.png'%(os.environ["dir_base"]))
